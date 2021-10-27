c04509adc295ecb588fd56f717db4a4e01c68596
Talles Carpes <42809152+TallesECB@users.noreply.github.com>
2021-10-27 09:51:45 -0300

    Finalizando as validações e suas mensagens, padronizando os status das requisições

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -5,7 +5,7 @@ module.exports = (server, routes, prefix = '/api/v1/car') => {
   routes.post('/', createValidation, CarController.create);
   routes.get('/', CarController.getAll);
   routes.get('/:idCar', CarController.getById);
-  routes.put('/:idCar', CarController.update);
+  routes.put('/:idCar', createValidation, CarController.update);
   routes.delete('/:idCar', CarController.remove);
 
   server.use(prefix, routes);
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -5,7 +5,7 @@ module.exports = (server, routes, prefix = '/api/v1/people') => {
   routes.post('/', createValidation, UserController.create);
   routes.get('/', UserController.getAll);
   routes.get('/:idPeople', UserController.getById);
-  routes.put('/:idPeople', UserController.update);
+  routes.put('/:idPeople', createValidation, UserController.update);
   routes.delete('/:idPeople', UserController.remove);
   
   server.use(prefix, routes);
